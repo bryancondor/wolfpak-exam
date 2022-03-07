793a11476559a43560dc3343315927d8faade0d7
Bryan Alexander Condor De La Cruz <bryanalexandercd@gmail.com>
2022-03-07 13:17:49 -0500

    update test and create README.md

---
+++ README.md
@@ -0,0 +1,21 @@
+# Wolfepak Assignment
+
+it's necessary to have a `node 12` version or higher.
+
+### ¿How I can pass input data?
+
+You should pass the data using the file `resources/input.csv`
+
+### ¿How I can execute the application?
+
+Only should run `npm start` in your terminal
+
+### ¿How I can run the test for the application?
+
+Only should run `npm test` in your terminal
+
+### ¿Where I can show the result?
+
+The result will print in the terminal
+
+_Created by **@bryancondor**_
--- test/shared/pickValuesThatCompletedMaxValue.test.js
+++ test/shared/pickValuesThatCompletedMaxValue.test.js
@@ -1,26 +1,113 @@
-const { pickValuesThatCompletedMaxValue } = require("../../src/shared/pickValuesThatCompletedMaxValue");
+const {
+  pickShippingsToPacking,
+} = require("../../src/shared/pickShippingsToPacking");
 
-describe('pickValuesThatCompletedMaxValue', () => {
-    it('should be return 2 elements that completed max weight', () => {
-        const result = pickValuesThatCompletedMaxValue([2, 3, 4, 5, 5], 10);
+describe("pickShippingsToPacking", () => {
+  it("should be return 2 elements that completed max weight", () => {
+    const result = pickShippingsToPacking(
+      [
+        { location: "Miraflores", weight: 2 },
+        { location: "San Martin de Pangoa", weight: 3 },
+        { location: "Arequipa", weight: 4 },
+        { location: "San Isidro", weight: 5 },
+        { location: "Huancayo", weight: 5 },
+      ],
+      10
+    );
 
-        expect(result).toEqual({ picked: [5, 5], unPicked: [4, 3, 2] });
+    expect(result).toEqual({
+      shippingsPicked: [
+        { location: "Huancayo", weight: 5 },
+        { location: "San Isidro", weight: 5 },
+      ],
+      shippingsUnpicked: [
+        { location: "Arequipa", weight: 4 },
+        { location: "San Martin de Pangoa", weight: 3 },
+        { location: "Miraflores", weight: 2 },
+      ],
     });
+  });
 
-    it('should return 4 elements that completed max weigth', () => {
-        const result = pickValuesThatCompletedMaxValue([2, 3, 4, 5, 5, 7, 2], 20);
+  it("should return 4 elements that completed max weigth", () => {
+    const result = pickShippingsToPacking(
+      [
+        { location: "Miraflores", weight: 2 },
+        { location: "San Martin de Pangoa", weight: 3 },
+        { location: "Arequipa", weight: 4 },
+        { location: "San Isidro", weight: 5 },
+        { location: "Huancayo", weight: 5 },
+        { location: "Libertad", weight: 7 },
+        { location: "Apurimac", weight: 2 },
+      ],
+      20
+    );
 
-        expect(result).toEqual({ picked: [7, 5, 5, 3], unPicked: [4, 2, 2] });
+    expect(result).toEqual({
+      shippingsPicked: [
+        { location: "Libertad", weight: 7 },
+        { location: "Huancayo", weight: 5 },
+        { location: "San Isidro", weight: 5 },
+        { location: "San Martin de Pangoa", weight: 3 },
+      ],
+      shippingsUnpicked: [
+        { location: "Arequipa", weight: 4 },
+        { location: "Apurimac", weight: 2 },
+        { location: "Miraflores", weight: 2 },
+      ],
     });
+  });
 
-    it('should return all elements given that are less than max', () => {
-        const result = pickValuesThatCompletedMaxValue([2, 3, 4, 5, 5, 7, 2], 30);
+  it("should return all elements given that are less than max", () => {
+    const result = pickShippingsToPacking(
+      [
+        { location: "Miraflores", weight: 2 },
+        { location: "San Martin de Pangoa", weight: 3 },
+        { location: "Arequipa", weight: 4 },
+        { location: "San Isidro", weight: 5 },
+        { location: "Huancayo", weight: 5 },
+        { location: "Libertad", weight: 7 },
+        { location: "Apurimac", weight: 2 },
+      ],
+      30
+    );
 
-        expect(result).toEqual({ picked: [7, 5, 5, 4, 3, 2, 2], unPicked: [] });
+    expect(result).toEqual({
+      shippingsPicked: [
+        { location: "Libertad", weight: 7 },
+        { location: "Huancayo", weight: 5 },
+        { location: "San Isidro", weight: 5 },
+        { location: "Arequipa", weight: 4 },
+        { location: "San Martin de Pangoa", weight: 3 },
+        { location: "Apurimac", weight: 2 },
+        { location: "Miraflores", weight: 2 },
+      ],
+      shippingsUnpicked: [],
     });
+  });
 
-    it('should return 2 elements given that elements in less than max', () => {
-        const result = pickValuesThatCompletedMaxValue([3, 3, 4, 7, 9], 15);
-        expect(result).toEqual({ picked: [9, 4], unPicked: [7, 3, 3] });
+  it("should return 2 elements given that elements in less than max", () => {
+    const result = pickShippingsToPacking(
+      [
+        { location: "Piura", weight: 3 },
+        { location: "Miraflores", weight: 3 },
+        { location: "San Martin de Pangoa", weight: 5 },
+        { location: "Arequipa", weight: 7 },
+        { location: "San Isidro", weight: 9 },
+        { location: "Huancayo", weight: 5 },
+      ],
+      15
+    );
+    expect(result).toEqual({
+      shippingsPicked: [
+        { location: "San Isidro", weight: 9 },
+        { location: "Huancayo", weight: 5 },
+      ],
+      shippingsUnpicked: [
+        { location: "Arequipa", weight: 7 },
+        { location: "San Martin de Pangoa", weight: 5 },
+        { location: "Miraflores", weight: 3 },
+        { location: "Piura", weight: 3 },
+      ],
     });
+  });
 });
